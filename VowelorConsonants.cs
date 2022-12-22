2fda06f94aea7829bdb6508ebebb7f42b558eebf
ronitshinde <rony.shinde753@gmail.com>
2022-12-22 14:09:56 +0530

    Basic vowel and consonants program

--- VowelorConsonants.cs
+++ VowelorConsonants.cs
@@ -11,14 +11,19 @@ namespace BasicCoreProgram
         public static void Alphabets()
         {
             Console.WriteLine("Enter any Character : ");
+
             char Alp =Convert.ToChar(Console.ReadLine());
+
 
             if (Alp == 'a' || Alp == 'e' || Alp == 'i' || Alp == 'o' || Alp == 'u' ||
                   Alp == 'A' || Alp == 'E' || Alp == 'I' || Alp == 'O' || Alp == 'U')
+
             {
                 Console.WriteLine(Alp + " Alphabet is vowels ");
             }
+
             else if ((Alp >= 'a' && Alp <= 'z') || (Alp >= 'A' && Alp <= 'Z'))
+
             { 
                 Console.WriteLine( Alp + " Alphabet is Consonant ");
             }
