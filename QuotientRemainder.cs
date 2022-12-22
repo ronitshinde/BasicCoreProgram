28082d98853bccc1b281148763eb506ce4be6186
ronitshinde <rony.shinde753@gmail.com>
2022-12-22 21:11:58 +0530

    Quotient and Remainder program

---
+++ QuotientRemainder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class QuotientRemainder
+    {
+        public static void QRemainder()
+        {
+            Console.WriteLine("Enter the value of dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = (dividend / divisor);
+            int remainder = (dividend % divisor);
+
+            //Console.WriteLine("Dividend : {0} Divisor : {1}" ,dividend,divisor);
+             Console.WriteLine("Dividend : " + dividend + " and Divisor : " + divisor);
+
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+        
+        }
+    }
+}
