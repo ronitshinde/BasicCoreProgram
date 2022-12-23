a33d7c4f7e9489b0b5281c2545b13502ac5ecf34
ronitshinde <rony.shinde753@gmail.com>
2022-12-23 14:24:03 +0530

    Swiping of two numbers program added

---
+++ SwipeNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class SwipeNumbers
+    {
+        public static void SwipeNum()
+        {
+            Console.WriteLine("Enter value of a");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter value of b");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Value before swap a = "+a +", b = " +b);
+            //Console.WriteLine("Value of b : " + a);
+
+            a = a * b;
+            b = a / b;
+            a = a / b;
+
+            Console.WriteLine("Value after swap a = " + a + ", b = " +b);
+        }    
+    }
+}
