bc4345a9206f6e5c5cf7cdb5dc5fa63f50cc31d1
ronitshinde <rony.shinde753@gmail.com>
2022-12-23 14:46:07 +0530

    Even Odd Number code added

---
+++ EvenOddNumber.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace BasicCoreProgram
+{
+    public class EvenOddNumber
+    {
+        public static void EvensOdds()
+        {
+            int a, b=2, c=0;
+            
+            Console.WriteLine("Enter the Number");
+            a = Convert.ToInt32(Console.ReadLine());
+            c = a % b;
+            
+            if (c == 0)
+            {
+                Console.WriteLine("Number is Even : " + a);
+            }
+            else
+            { 
+                Console.WriteLine("Number is odd : " + a);
+            }
+        }
+    }
+}
